7d5c00c0db75d4a2635c1fe018ae3bfc9ba662ef
yuchen-7-d <yuchen-7-d@users.noreply.github.com>
2026-09-12 07:04:10 +0800

    发送目标 → 仿真执行 → 读取实际位置 → 判断误差

--- main.py
+++ main.py
@@ -23,13 +23,50 @@ def main():
     data = mujoco.MjData(model)
     start_time = data.time
     print(f'推进前时间:{start_time:.6f}s')
+
+    head_pan_actuator_id = mujoco.mj_name2id(
+        model,
+        mujoco.mjtObj.mjOBJ_ACTUATOR,
+        "head_pan"
+    )
+
+    actuator_ids = {"head_pan": head_pan_actuator_id}
+
+    for actuator_name, actuator_id in actuator_ids.items():
+        if actuator_id == -1:
+            raise RuntimeError(f'找不到Mujoco actuator: {actuator_name}')
+        print(
+            f'actuator name = {actuator_name}', 
+            f'actuator id = {actuator_id}'
+        )
+
+    head_pan_ctrl_range = model.actuator_ctrlrange[
+        head_pan_actuator_id
+    ]
+
+    target_angle = 0.3
+
+    lower_limit, upper_limit = head_pan_ctrl_range
+
+    if not (lower_limit <= target_angle <=upper_limit):
+        raise ValueError(f'目标角度超出控制范围:{target_angle}')
+
     with mujoco.viewer.launch_passive(model, data) as viewer:
         while viewer.is_running():
+            data.ctrl[head_pan_actuator_id] = target_angle
             mujoco.mj_step(model, data)
             viewer.sync()
             time.sleep(model.opt.timestep)
 
     print(f'推进后时间:{data.time:.6f}s')
+
+    joint_state = data.joint('joint_head_pan')
+    actual_angle = joint_state.qpos[0]
+    angle_error = actual_angle - target_angle
+
+    print(f'目标角度:{target_angle:.6f} rad')
+    print(f'实际角度:{actual_angle:.6f} rad')
+    print(f'角度误差:{angle_error:.6f} rad')
 
 
 if __name__ == '__main__':
