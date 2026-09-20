f8c4236a3f8122e41b3caab8c2133f13b923c685
yuchen-7-d <yuchen-7-d@users.noreply.github.com>
2026-09-20 08:54:19 +0800

    拍摄环节

--- camera_probe.py
+++ camera_probe.py
@@ -15,7 +15,6 @@ def main():
 
     model = mujoco.MjModel.from_xml_path(str(camera_probe_path))
     data = mujoco.MjData(model)
-    strat_time = data.time
 
     d435i_camera_rgb_id = mujoco.mj_name2id(
         model,
@@ -32,6 +31,28 @@ def main():
     print(f'相机数量:{model.ncam}')
     print(f'目标相机:{camera_name}')
     print(f'相机ID:{d435i_camera_rgb_id}')
+
+    data.joint('joint_head_pan').qpos[0] = -1.57
+    data.joint('joint_head_tilt').qpos[0] = -0.9
+
+    mujoco.mj_forward(model, data)
+
+    with mujoco.Renderer(model, width=640, height=480) as renderer:
+        renderer.update_scene(data, camera=d435i_camera_rgb_id)
+        rgb_image = renderer.render()
+
+    bgr_image = cv.cvtColor(rgb_image, cv.COLOR_RGB2BGR)
+
+    image_path = Path('/home/yu/stretch_rebuild/saved_picture/camera_rgb.png')
+    saved = cv.imwrite(str(image_path), bgr_image)
+
+    if not saved:
+        raise RuntimeError(f'图片保存失败:{image_path}')
+
+    print(f'图像状态:{rgb_image.shape}')
+    print(f'图像类型:{rgb_image.dtype}')
+    print(f'保存状态:{saved}')
+    print(f'保存至:{image_path}')
 
 
 if __name__ == '__main__':
